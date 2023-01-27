c4a99f7fc797fd2e5e9bd770c276ed54cb9d7c95
Owais Tahir <oo_wais@hotmail.com>
2023-01-27 16:19:01 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -51,11 +51,6 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 #############################functions#################################
-def apply_scale_factors(image):
-    opticalBands = image.select('SR_B.').multiply(0.0000275).add(-0.2)
-    thermalBands = image.select('ST_B.*').multiply(0.00341802).add(149.0)
-    return image.addBands(opticalBands, None, True).addBands(thermalBands, None, True)
-
 #load model function, set cache to prevent reloading
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -71,6 +66,36 @@ def preprocess_image(img):
     img = np.expand_dims(img, axis=0)
     return img
 
+#downlaod image function
+def download_image():
+    image_collection = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2") \
+        .filterDate(start_date, end_date) \
+        .filterBounds(geometry)
+
+    median = image_collection.median()
+    dataset = apply_scale_factors(median)
+    out_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
+    filename = os.path.join(out_dir, 'image.tif')
+    imageRGB = dataset.visualize(**{'bands': ['SR_B4', 'SR_B3', 'SR_B2'], 'min':0.0, 'max': 0.3})
+    geemap.ee_export_image(imageRGB,filename=filename,scale= 50, region=geometry)
+    st.success('Image sotred in /Downloads')
+
+#apply scaling factors to the downloaded image
+def apply_scale_factors(image):
+    opticalBands = image.select('SR_B.').multiply(0.0000275).add(-0.2)
+    thermalBands = image.select('ST_B.*').multiply(0.00341802).add(149.0)
+    return image.addBands(opticalBands, None, True).addBands(thermalBands, None, True)
+
+#calculate precipitation function
+@st.cache(allow_output_mutation=True)
+def calculate_precipitation():
+    chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD")
+    filtered = chirps.filter(ee.Filter.date(start_date, end_date))
+    total = filtered.reduce(ee.Reducer.sum()).clip(geometry) #Calculate rainfall over flood period
+    precipitation_stats = geemap.image_stats(total, scale=5000) #Calculate average rainfall across a region
+    precipitation_stats_values = precipitation_stats.getInfo()
+    return precipitation_stats_values
+
 ########################## instructions ###############################
 st.write('1. Select a region that is affected by floods on the map.\n2. Click "Export" button on the upper right corner of the map to download the coordinates of the region in a json file.\n3. Set the date range.\n4. Upload the json file that you downloaded in step 2. An RGB image of the selected region will be downloaded. \n5. Upload the image back to get the flood mapping of the region.')
 
@@ -83,7 +108,7 @@ with row1_col1:
     m.add_basemap('ROADMAP')
     m.to_streamlit(height=530)
 
-#Date Sliders
+#Date Widgets
 with row1_col2: 
     start_date= st.date_input(
                 label='Start Date',
@@ -102,63 +127,30 @@ with row1_col2:
     end_date=str(end_date)
 
     uploaded_file = st.file_uploader("Upload the GeoJSON file")
-
     geometry=None
-
     if uploaded_file is not None:
         #ee.Authenticate()
         ee.Initialize()
-        # Open the JSON file
-        geojson = json.loads(uploaded_file.read())
+        geojson = json.loads(uploaded_file.read()) #open geojson file
+        parsed_json = json.loads(json.dumps(geojson)) # Parse the GeoJSON object
+        coordinates = parsed_json['features'][0]['geometry']['coordinates'][0] # Extract the coordinates from the GeoJSON object
+        geometry = ee.Geometry.Polygon(coordinates) # Create an ee.Geometry object using the coordinates
+        
+        with st.spinner('Calculating Precipitation...'):
+            precipitation=calculate_precipitation() #calculate precipation for the region of interest
+            st.metric(label = 'Precipitation', value = round(precipitation['mean']['precipitation_sum']))
 
-        # Parse the GeoJSON object
-        parsed_json = json.loads(json.dumps(geojson))
-
-        # Extract the coordinates from the GeoJSON object
-        coordinates = parsed_json['features'][0]['geometry']['coordinates'][0]
-
-        # Create an ee.Geometry object using the coordinates
-        geometry = ee.Geometry.Polygon(coordinates)
-
-    ########################download image########################
     with st.spinner('Downloading Image...'):
         if st.button('Download Image'):
-            image_collection = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2") \
-                .filterDate(start_date, end_date) \
-                .filterBounds(geometry)
-
-            median = image_collection.median()
-            dataset = apply_scale_factors(median)
-            out_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
-            filename = os.path.join(out_dir, 'image.tif')
-            imageRGB = dataset.visualize(**{'bands': ['SR_B4', 'SR_B3', 'SR_B2'], 'min':0.0, 'max': 0.3})
-            geemap.ee_export_image(imageRGB,filename=filename,scale= 50, region=geometry)
-            st.success('Image sotred in /Downloads')
-
-    # with st.spinner('Calculating Precipitation...'):
-    #         chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD")
-    #         filtered = chirps.filter(ee.Filter.date(start_date, end_date))
-    #         #Calculate rainfall over flood period
-    #         total = filtered.reduce(ee.Reducer.sum()).clip(geometry)
-    #         #Calculate average rainfall across a region
-    #         precipitation_stats = geemap.image_stats(total, scale=5000)
-    #         precipitation_stats_values = precipitation_stats.getInfo()
-    #         st.metric(label = 'Precipitation (mm)', value = round(precipitation_stats_values['mean']['precipitation_sum']))
-
-######################################################################
-
-
-################################################################################
+            download_image()
 
 with st.spinner("Loading Model...."):
     model = load_model() #call load_model function to load the model
 
-#ask user to upload image
-file = st.file_uploader("Upload the downloaded image ", type=['tif'])
+file = st.file_uploader("Upload the downloaded image ", type=['tif']) #ask user to upload image
 
 if file is not None:
     image = Image.open(file) #open uploaded image
-
     prepd_img=preprocess_image(image) #preprocess image to meet the model's input requirements
     original_img= prepd_img.reshape((128,128,3))
     original_img= cv2.resize(original_img,(400,400))
@@ -173,13 +165,6 @@ if file is not None:
     col1.image(original_img)
     col2.image(prediction_image)
     
-    with col3:
-        with st.spinner('Calculating Precipitation...'):
-            chirps = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD")
-            filtered = chirps.filter(ee.Filter.date(start_date, end_date))
-            #Calculate rainfall over flood period
-            total = filtered.reduce(ee.Reducer.sum()).clip(geometry)
-            #Calculate average rainfall across a region
-            precipitation_stats = geemap.image_stats(total, scale=5000)
-            precipitation_stats_values = precipitation_stats.getInfo()
-            st.metric(label = 'Precipitation (mm)', value = round(precipitation_stats_values['mean']['precipitation_sum']))
+    #with col3:
+        # with st.spinner('Calculating Precipitation...'):
+        #     calculate_precipitation() #calculate precipation for the region of interest
