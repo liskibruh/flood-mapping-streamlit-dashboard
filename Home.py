b51a6606fb6ac46efceb014a2f47f73d324d588f
Owais Tahir <oo_wais@hotmail.com>
2024-04-18 15:05:55 +0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,2 +1,2 @@
 # this script is not doing anything. the main python scripts are in the "pages" directory
-# the program will still run in the same way as mentioned in the repo description (README.md)
+# the project will still run in the same way as mentioned in the repo description (README.md)
